4843aab31c984542ecdcee189e4e11c3ba950227
Jesús Jerónimo Guevara Rogel <85355797+guevararogeljj@users.noreply.github.com>
2023-09-28 14:07:45 -0600

    se actualiza donav2.sql

--- DonoutsWeb/donoutsV2.sql
+++ DonoutsWeb/donoutsV2.sql
@@ -1,6 +1,6 @@
 USE [Donas]
 GO
-/****** Object:  Table [dbo].[__EFMigrationsHistory]    Script Date: 27/09/2023 08:48:18 p. m. ******/
+/****** Object:  Table [dbo].[__EFMigrationsHistory]    Script Date: 28/09/2023 02:05:46 p. m. ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -14,7 +14,7 @@ CREATE TABLE [dbo].[__EFMigrationsHistory](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[ACTIVIDADES]    Script Date: 27/09/2023 08:48:18 p. m. ******/
+/****** Object:  Table [dbo].[ACTIVIDADES]    Script Date: 28/09/2023 02:05:46 p. m. ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -34,7 +34,7 @@ CREATE TABLE [dbo].[ACTIVIDADES](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[AspNetRoleClaims]    Script Date: 27/09/2023 08:48:18 p. m. ******/
+/****** Object:  Table [dbo].[AspNetRoleClaims]    Script Date: 28/09/2023 02:05:46 p. m. ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -50,7 +50,7 @@ CREATE TABLE [dbo].[AspNetRoleClaims](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[AspNetRoles]    Script Date: 27/09/2023 08:48:18 p. m. ******/
+/****** Object:  Table [dbo].[AspNetRoles]    Script Date: 28/09/2023 02:05:46 p. m. ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -66,7 +66,7 @@ CREATE TABLE [dbo].[AspNetRoles](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[AspNetUserClaims]    Script Date: 27/09/2023 08:48:18 p. m. ******/
+/****** Object:  Table [dbo].[AspNetUserClaims]    Script Date: 28/09/2023 02:05:46 p. m. ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -82,7 +82,7 @@ CREATE TABLE [dbo].[AspNetUserClaims](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[AspNetUserLogins]    Script Date: 27/09/2023 08:48:18 p. m. ******/
+/****** Object:  Table [dbo].[AspNetUserLogins]    Script Date: 28/09/2023 02:05:46 p. m. ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -99,7 +99,7 @@ CREATE TABLE [dbo].[AspNetUserLogins](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[AspNetUserRoles]    Script Date: 27/09/2023 08:48:18 p. m. ******/
+/****** Object:  Table [dbo].[AspNetUserRoles]    Script Date: 28/09/2023 02:05:46 p. m. ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -114,7 +114,7 @@ CREATE TABLE [dbo].[AspNetUserRoles](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[AspNetUsers]    Script Date: 27/09/2023 08:48:18 p. m. ******/
+/****** Object:  Table [dbo].[AspNetUsers]    Script Date: 28/09/2023 02:05:46 p. m. ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -145,7 +145,7 @@ CREATE TABLE [dbo].[AspNetUsers](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[AspNetUserTokens]    Script Date: 27/09/2023 08:48:18 p. m. ******/
+/****** Object:  Table [dbo].[AspNetUserTokens]    Script Date: 28/09/2023 02:05:46 p. m. ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -163,7 +163,7 @@ CREATE TABLE [dbo].[AspNetUserTokens](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[CALENDARIOACTIVIDADES]    Script Date: 27/09/2023 08:48:18 p. m. ******/
+/****** Object:  Table [dbo].[CALENDARIOACTIVIDADES]    Script Date: 28/09/2023 02:05:46 p. m. ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -191,7 +191,7 @@ CREATE TABLE [dbo].[CALENDARIOACTIVIDADES](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[TIPO_DONAS]    Script Date: 27/09/2023 08:48:18 p. m. ******/
+/****** Object:  Table [dbo].[TIPO_DONAS]    Script Date: 28/09/2023 02:05:46 p. m. ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -209,7 +209,7 @@ CREATE TABLE [dbo].[TIPO_DONAS](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[VENTAS_DONAS]    Script Date: 27/09/2023 08:48:18 p. m. ******/
+/****** Object:  Table [dbo].[VENTAS_DONAS]    Script Date: 28/09/2023 02:05:46 p. m. ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -232,6 +232,22 @@ CREATE TABLE [dbo].[VENTAS_DONAS](
 ) ON [PRIMARY]
 GO
 INSERT [dbo].[__EFMigrationsHistory] ([MigrationId], [ProductVersion]) VALUES (N'20230928024537_Initial', N'7.0.4')
+GO
+INSERT [dbo].[ACTIVIDADES] ([Id], [CreatedBy], [LastUpdatedBy], [Visible], [CreatedAt], [ModifiedAt], [Code], [Name]) VALUES (N'21541fe2-2425-48b4-1ce5-08dbc05df958', N'00000000-0000-0000-0000-000000000000', N'', 1, CAST(N'2023-09-28T14:03:23.6138283' AS DateTime2), CAST(N'2023-09-28T14:03:23.6138961' AS DateTime2), N'Actividad 1', N'Actividad 1')
+GO
+INSERT [dbo].[ACTIVIDADES] ([Id], [CreatedBy], [LastUpdatedBy], [Visible], [CreatedAt], [ModifiedAt], [Code], [Name]) VALUES (N'ce73d7a0-4288-43ed-1ce6-08dbc05df958', N'00000000-0000-0000-0000-000000000000', N'', 1, CAST(N'2023-09-28T14:03:34.5628559' AS DateTime2), CAST(N'2023-09-28T14:03:34.5628580' AS DateTime2), N'Actividad 2', N'Actividad 2')
+GO
+INSERT [dbo].[AspNetRoles] ([Id], [Name], [NormalizedName], [ConcurrencyStamp]) VALUES (N'e474fc2b-b353-46dd-977e-eb4ae386a05d', N'Admin', N'ADMIN', NULL)
+GO
+INSERT [dbo].[AspNetRoles] ([Id], [Name], [NormalizedName], [ConcurrencyStamp]) VALUES (N'6f00204a-e189-4d36-81ad-ed87560b53a7', N'Cliente', N'CLIENTE', NULL)
+GO
+INSERT [dbo].[AspNetUserRoles] ([UserId], [RoleId]) VALUES (N'5e49ddc6-df2c-4d38-ecda-08dbc03aa5be', N'e474fc2b-b353-46dd-977e-eb4ae386a05d')
+GO
+INSERT [dbo].[AspNetUserRoles] ([UserId], [RoleId]) VALUES (N'230698b5-4209-4776-ecdb-08dbc03aa5be', N'6f00204a-e189-4d36-81ad-ed87560b53a7')
+GO
+INSERT [dbo].[AspNetUsers] ([Id], [Name], [CreatedAt], [ModifiedAt], [Direction], [UserName], [NormalizedUserName], [Email], [NormalizedEmail], [EmailConfirmed], [PasswordHash], [SecurityStamp], [ConcurrencyStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEnd], [LockoutEnabled], [AccessFailedCount]) VALUES (N'5e49ddc6-df2c-4d38-ecda-08dbc03aa5be', N'Jeronimo Guevara', N'2023-09-28 09:50:32.2423793', CAST(N'2023-09-28T09:50:32.2424959' AS DateTime2), N'Ave. central #397', N'JJGR', N'JJGR', N'guevararogeljj@gmail.com', N'GUEVARAROGELJJ@GMAIL.COM', 0, N'AQAAAAIAAYagAAAAEBDts4gV5VWSFYDMJT0rEWIWJXRum+bjzKS2jqiyegUonLn0FeHqG3m4a9+hdTHKfw==', N'MSMWB552G5U4FW5ZKVXXIXGENWMNUW6O', N'7f0bb55d-31f6-4fe1-a066-60c85a585cb4', N'5521938494', 0, 0, NULL, 1, 0)
+GO
+INSERT [dbo].[AspNetUsers] ([Id], [Name], [CreatedAt], [ModifiedAt], [Direction], [UserName], [NormalizedUserName], [Email], [NormalizedEmail], [EmailConfirmed], [PasswordHash], [SecurityStamp], [ConcurrencyStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEnd], [LockoutEnabled], [AccessFailedCount]) VALUES (N'230698b5-4209-4776-ecdb-08dbc03aa5be', N'Jeronimo Guevara Cliente', N'2023-09-28 09:57:32.1959375', CAST(N'2023-09-28T09:57:32.1959401' AS DateTime2), N'Ave. central #397', N'JJGR2', N'JJGR2', N'guevararogeljj@hotmail.com', N'GUEVARAROGELJJ@HOTMAIL.COM', 0, N'AQAAAAIAAYagAAAAEArqeCHaJacMGju845faep5LmdzZR8xzRA6OnLdPFdpkkQEd4VFNYhPsvbulprhrLg==', N'MTJ23FTEYJMRELJUX74VEUKSIIDRO2HE', N'de609d2f-5b8e-4fd9-89e9-c75da0561092', N'5521938494', 0, 0, NULL, 1, 0)
 GO
 ALTER TABLE [dbo].[AspNetRoleClaims]  WITH CHECK ADD  CONSTRAINT [FK_AspNetRoleClaims_AspNetRoles_RoleId] FOREIGN KEY([RoleId])
 REFERENCES [dbo].[AspNetRoles] ([Id])
